391c85045c88bc70ded6eda9f2aefbcbb143a10e
Hariumasankar <hariumasankarcse@gmail.com>
2025-02-11 20:59:56 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -18,7 +18,7 @@ export default function Contact() {
     );
     
     // Replace with your WhatsApp number
-    const whatsappNumber = '+918609271849';
+    const whatsappNumber = '8609271849';
     
     // Open WhatsApp with the pre-filled message
     window.open(`https://wa.me/${whatsappNumber}?text=${whatsappMessage}`, '_blank');
